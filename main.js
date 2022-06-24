ee0491e28a7ced80b53b08cac5066b9fc7d47d7e
Sarbjit <devcodetest1@gmail.com>
2022-06-24 12:56:30 +0100

    express server is create

---
+++ main.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const httpServer = http.createServer(app);
+const path = require("path");
+const fs = require("fs");
+const port = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, "static")));
+httpServer.listen(port, () => {
+  console.log(`http server is listening on ${port}`);
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
